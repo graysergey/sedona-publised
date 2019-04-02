7aca7ee9d711808830a08fe822d1838cc93df646
GraySergey <xgortx@gmail.com>
2019-04-02 18:46:05 +0300

    Создает репозиторий

---
+++ js/menu.js
@@ -0,0 +1,1 @@
+var toggle=document.querySelector(".page-header__menu-toggle"),none=document.querySelector(".site-list");none.classList.add("site-list--close"),toggle.addEventListener("click",function(e){e.preventDefault(),toggle.classList.toggle("page-header__menu-toggle--close"),none.classList.toggle("site-list--close")});
